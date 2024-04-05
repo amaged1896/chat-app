810005d584b08b84ef9f09ccbe790cfa1b0b8341
amaged1896 <ahmed.maged1896@gmail.com>
2024-04-05 02:00:57 +0200

    chat-activity-detection

--- server/public/app.js
+++ server/public/app.js
@@ -1,13 +1,14 @@
 const socket = io('ws://localhost:3500');
+const activity = document.querySelector('.activity');
+const msgInput = document.querySelector('input');
 
 function sendMessage(e) {
     e.preventDefault();
-    const input = document.querySelector('input');
-    if (input.value) {
-        socket.emit("message", input?.value);
-        input.value = '';
+    if (msgInput.value) {
+        socket.emit("message", msgInput?.value);
+        msgInput.value = '';
     }
-    input.focus();
+    msgInput.focus();
 }
 
 document.querySelector('form')
@@ -19,3 +20,15 @@ socket.on('message', (data) => {
     li.textContent = data;
     document.querySelector('ul').appendChild(li);
 });
+
+
+msgInput.addEventListener('keypress', () => {
+    socket.emit('activity', socket.id.substring(0, 5));
+});
+
+socket.on('activity', (name) => {
+    activity.textContent = `${name} is typing...`;
+    setTimeout(() => {
+        activity.textContent = '';
+    }, 2000);
+})
